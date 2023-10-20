803c46d5f21cbdebf189e928292f47983957a429
Danila Zaharkin <u.mka@bk.ru>
2023-10-20 03:23:53 +0300

    дщдулал текст бокс и добавил окно

--- Kursach/Window1.xaml.cs
+++ Kursach/Window1.xaml.cs
@@ -10,58 +10,58 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Kursach
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
-        }   
-
-        
-
-        private void Button_Enter_Click(object sender, RoutedEventArgs e)
-        {
-            new Window2().Show();
-            Hide();
         }
-
 
         private void TextBoxEmail_GotFocus(object sender, RoutedEventArgs e)
         {
-            if (TextBoxEmail.Text != "Введите почту...") return;
             TextBoxEmail.Text = "";
-            TextBoxEmail.Foreground = new SolidColorBrush(Colors.Black);
         }
 
         private void TextBoxEmail_LostFocus(object sender, RoutedEventArgs e)
         {
-            if (TextBoxEmail.Text != "") return;
             TextBoxEmail.Text = "Введите почту...";
-            TextBoxEmail.Foreground = new SolidColorBrush(Color.FromRgb(220, 220, 220));
+        }
+
+        private void TextBoxEmail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_SetUp_Click(object sender, RoutedEventArgs e)
+        {
+
         }
 
         private void TextBoxPassword_GotFocus(object sender, RoutedEventArgs e)
         {
-            if (TextBoxPassword.Text != "Введите пароль...") return;
             TextBoxPassword.Text = "";
-            TextBoxPassword.Foreground = new SolidColorBrush(Colors.Black);
         }
 
         private void TextBoxPassword_LostFocus(object sender, RoutedEventArgs e)
         {
-            if (TextBoxPassword.Text != "") return;
             TextBoxPassword.Text = "Введите пароль...";
-            TextBoxPassword.Foreground = new SolidColorBrush(Color.FromRgb(220, 220, 220));
         }
 
-       
+        private void TextBoxConfirmPassword_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBoxConfirmPassword.Text = "";
+        }
+
+        private void TextBoxConfirmPassword_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBoxConfirmPassword.Text = "Подтвердите пароль";
+        }
     }
 }
